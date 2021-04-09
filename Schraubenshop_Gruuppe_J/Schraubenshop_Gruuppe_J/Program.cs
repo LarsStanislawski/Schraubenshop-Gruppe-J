8aff24138c98203dd746966622e9e2d302d1de14
LarsStanislawski <larsstanislawski@web.de>
2021-04-09 13:51:20 +0200

    Grundstruktur
    erste gemeinsame Schritte

---
+++ Schraubenshop_Gruuppe_J/Schraubenshop_Gruuppe_J/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schraubenshop_Gruuppe_J
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Willkommen im Schraubenshop!");    //Begrüßung
+            auswahlSM();
+
+            //Ausgabe
+            //Preis etc.
+        }
+
+        static void auswahlSM ()
+        {
+            int auswahl;                                                   
+            Console.WriteLine("Wollen Sie eine Mutter oder Schraube kaufen?");
+            Console.WriteLine("Für Schrauben geben Sie bitte die 1, für Muttern bitte die 2 ein");
+            auswahl = Convert.ToInt32(Console.ReadLine());
+
+            if (auswahl == 1)
+            {
+                Console.WriteLine("Schrauben");
+                schraube();
+            }
+            else if (auswahl == 2)
+            {
+                Console.WriteLine("Muttern");
+                muttern();               
+            }
+            else
+            {
+                Console.WriteLine("Bitte wählen Sie eine der beiden vorgeschlagenen Möglichkeiten aus!");
+                auswahlSM();
+            }
+        }
+        static void schraube()
+        {
+            int auswahl;
+            Console.WriteLine("Norm oder Sonderanfertigung?");
+            Console.WriteLine("Für Norm geben Sie bitte die 1, für Sonderanfertigung bitte die 2 ein. Mit 3 gelangen Sie zum Hauptmenü!");
+            auswahl = Convert.ToInt32(Console.ReadLine());
+            if (auswahl == 1)
+            {
+                Console.WriteLine("Norm");
+                schraube();
+            }
+            else if (auswahl == 2)
+            {
+                Console.WriteLine("Sonderanfertigung");
+                muttern();
+            }
+            else if (auswahl == 3)
+            {
+                auswahlSM();
+            }
+            else
+            {
+                Console.WriteLine("Bitte wählen Sie eine der vorgeschlagenen Möglichkeiten aus!");
+                schraube();
+            }
+        }
+        static void muttern ()
+        {
+            int auswahl;
+            Console.WriteLine("Norm oder Sonderanfertigung?");
+            Console.WriteLine("Für Norm geben Sie bitte die 1, für Sonderanfertigung bitte die 2 ein");
+            auswahl = Convert.ToInt32(Console.ReadLine());
+            if (auswahl == 1)
+            {
+                Console.WriteLine("Norm");
+                schraube();
+            }
+            else if (auswahl == 2)
+            {
+                Console.WriteLine("Sonderanfertigung");
+                muttern();
+            }
+            else
+            {
+                Console.WriteLine("Bitte wählen Sie eine der beiden vorgeschlagenen Möglichkeiten aus!");
+                schraube();
+            }
+
+
+        }
+
+        static void SechskantZylinderkopf ()
+        {
+
+        }
+
+        static void Zylinderkopf ()
+        {
+
+        }
+
+        static void Sechskant()
+        {
+
+        }
+        
+
+        static void SonderanfertigungSchraube ()
+        {
+
+        }
+
+
+
+    }
+}
